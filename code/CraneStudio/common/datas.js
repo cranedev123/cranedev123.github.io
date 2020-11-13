87f32e27ad807927937909be7685bd625596c0b0
crane <xyyh0116@aliyun.com>
2020-11-13 16:04:02 +0800

    update

---
+++ code/CraneStudio/common/datas.js
@@ -0,0 +1,79 @@
+var appList = {
+	"appList": [{
+			"name": "Railway Worker",
+			"name_zh": "Railway Worker",
+			"package": "cn.crane.flutter.plumber.lineworker",
+			"icon": "https://is2-ssl.mzstatic.com/image/thumb/Purple113/v4/46/e6/18/46e618e4-e002-a48f-9458-aec0cd5a752a/AppIcon-0-1x_U007emarketing-0-0-85-220-7.png/230x0w.png",
+			"link": "https://itunes.apple.com/app/1485420263"
+		},
+		{
+			"name": "Jigsaw World",
+			"name_zh": "多多数字拼图",
+			"package": "cn.crane.flutter.puzzle.jigsaw",
+			"icon": "https://is3-ssl.mzstatic.com/image/thumb/Purple123/v4/57/e3/5f/57e35f71-9796-ebc6-e119-ce61e33fd07a/AppIcon-0-1x_U007emarketing-0-0-85-220-7.png/738x0w.jpg",
+			"link": "https://itunes.apple.com/app/id1477751457"
+		},
+		{
+			"name": "Dance Up",
+			"name_zh": "一起来跳舞",
+			"package": "cn.crane.flutter.danceup",
+			"icon": "https://is1-ssl.mzstatic.com/image/thumb/Purple113/v4/e8/1a/1a/e81a1a2d-a106-1892-6091-3d54d30b8619/AppIcon-0-1x_U007emarketing-0-0-85-220-7.png/230x0w.jpg",
+			"link": "https://itunes.apple.com/app/id1475789889"
+		},
+		{
+			"name": "Crazy Sudoku",
+			"name_zh": "一起来数独",
+			"package": "cn.crane.myapp.flutterSudoku",
+			"icon": "https://is5-ssl.mzstatic.com/image/thumb/Purple113/v4/4e/c6/fe/4ec6fe35-0343-3cce-b6bf-ab3f6710d663/AppIcon-0-1x_U007emarketing-0-0-85-220-10.png/230x0w.jpg",
+			"link": "https://itunes.apple.com/app/id1465370843"
+		},
+		{
+			"name": "Touch Number",
+			"name_zh": "按数字",
+			"package": "cn.crane.game.flutter.findx",
+			"icon": "https://is1-ssl.mzstatic.com/image/thumb/Purple113/v4/e4/58/0a/e4580aa1-6848-78bb-2f47-0f69bf69cbc5/AppIcon-0-1x_U007emarketing-0-0-85-220-7.png/230x0w.jpg",
+			"link": "https://itunes.apple.com/app/id1481699832"
+		},
+		{
+			"name": "Flappy UFO",
+			"name_zh": "Flappy UFO",
+			"package": "cn.crane.game.flappyufo",
+			"icon": "https://is3-ssl.mzstatic.com/image/thumb/Purple113/v4/6d/6b/0c/6d6b0cb3-c4f3-5f3c-d1fd-0ac9dcda0748/AppIcon-0-1x_U007emarketing-0-0-85-220-7.png/230x0w.jpg",
+			"link": "https://itunes.apple.com/app/id1482524329"
+		}
+	]
+};
+
+var contact_list = [{
+	'title': 'Weibo',
+	'icon': 'Weibo',
+	'url': 'https://cranedev123.github.io/release/privacy.html'
+}, {
+	'title': 'Facebook',
+	'icon': 'Facebook',
+	'url': 'https://cranedev123.github.io/release/tos.html'
+}, {
+	'title': 'Twitter',
+	'icon': 'Twitter',
+	'url': 'https://cranedev123.github.io/release/tos.html'
+}, ];
+
+
+var links_bottom = [{
+	'title': 'Privacy Policy',
+	'url': 'https://cranedev123.github.io/release/privacy.html'
+}, {
+	'title': 'Terms of Service',
+	'url': 'https://cranedev123.github.io/release/tos.html'
+}, ];
+module.exports = {
+	getAppList() {
+		return appList
+	},
+	getContactList() {
+		return contact_list
+	},
+	getLinks_bottom() {
+		return links_bottom
+	},
+}
--- code/CraneStudio/pages/index/index.vue
+++ code/CraneStudio/pages/index/index.vue
@@ -1,60 +1,100 @@
 <template>
+
 	<view class="content">
 		<!-- <image class="logo" src="/static/logo.png"></image> -->
-		<view class="text-area">
+		<!-- <view class="text-area">
 			<text class="title">{{title}}</text>
-		</view>
+		</view> -->
 		<!-- <view class="bottom" v-for="item in links_bottom">
 			<text class="item_link" @click="onLinkClicked" v-bind:id="item.url">{{item.title}}</text>
 		</view> -->
 
-		<view class="contact">
 
-		</view>
+		<view class="bg" />
+
+
+		<uni-grid class="grid" :column="pro_colum" :showBorder="false">
+			<view class="uni-list-cell" hover-class="uni-list-cell-hover" v-for="(item,index) in productList" v-bind:id="index"
+			 :key="index" @click="onClickItem">
+				<uni-grid-item class="item_view1" :style="'width: '+itemW+'px; height: '+itemW+'px;'">
+					<view class="item_view">
+						<view class="item_icon">
+							<image v-if="renderImage" class="item_icon" mode="aspectFit" src="https://p.moimg.net/project/project_20201026_1603690116_3710.jpg?imageMogr2/auto-orient/strip"></image>
+<!-- 							<image v-if="renderImage" class="item_icon" mode="aspectFit" :src="item.icon"></image> -->
+						</view>
+						<view class="item_title">{{item.name}}</view>
+					</view>
+
+
+				</uni-grid-item>
+
+			</view>
+		</uni-grid>
+
 
 		<view class="bottom">
-			<text class="item_link" @click="onLinkClicked" v-bind:id="links_bottom[0].url">{{links_bottom[0].title}}</text>
-			<text class="item_link"> | </text>
-			<text class="item_link" @click="onLinkClicked" v-bind:id="links_bottom[1].url">{{links_bottom[1].title}}</text>
+			<view class="tos">
+				<text class="item_link" @click="onLinkClicked" v-bind:id="links_bottom[0].url">{{links_bottom[0].title}}</text>
+				<text class="item_link"> | </text>
+				<text class="item_link" @click="onLinkClicked" v-bind:id="links_bottom[1].url">{{links_bottom[1].title}}</text>
+			</view>
+			<view class="contact">
+			</view>
+			<view class="copyright">Copyright © 2018-2020 CraneStudio, Inc. All rights reserved.</view>
 		</view>
+
+
 	</view>
 </template>
 
 <script>
+	var Datas = require('../../common/datas.js')
 	export default {
 		data() {
 			return {
-				title: 'Hello',
-				links_bottom: [{
-					'title': 'Privacy Policy',
-					'url': 'https://cranedev123.github.io/release/privacy.html'
-				}, {
-					'title': 'Terms of Service',
-					'url': 'https://cranedev123.github.io/release/tos.html'
-				}, ],
-				contact_list: [{
-					'title': 'Weibo',
-					'icon': 'Weibo',
-					'url': 'https://cranedev123.github.io/release/privacy.html'
-				}, {
-					'title': 'Facebook',
-					'icon': 'Facebook',
-					'url': 'https://cranedev123.github.io/release/tos.html'
-				}, {
-					'title': 'Twitter',
-					'icon': 'Twitter',
-					'url': 'https://cranedev123.github.io/release/tos.html'
-				}, ]
+				productList: [{}],
+				renderImage: true,
+				pro_colum: 4,
+				title: 'Hello111',
+				platform: 'other',
+				isZh:false,
+				links_bottom: [],
+				contact_list: [],
+				itemW: 50
 			}
 		},
 		onLoad() {
-
+			var info = uni.getSystemInfoSync()
+			this.platform = info.platform
+			this.isZh = info.language && info.language.indexOf('zh') >= 0
+			
+			if(this.platform == 'android' || this.platform == 'ios')
+			{
+				this.pro_colum = 2
+				this.itemW = info.screenWidth / 2
+			}else{
+				this.pro_colum = 5
+				this.itemW = info.screenWidth / 6
+			}
+			console.log(info)
+			
+			this.getProductList()
 		},
 		methods: {
 			onLinkClicked(e) {
 				var url = e.currentTarget.id
 				window.location.href = url
-			}
+			},
+			getProductList() {
+				this.productList = Datas.getAppList().appList
+				this.contact_list = Datas.getContactList()
+				this.links_bottom = Datas.getLinks_bottom()
+			},
+			onClickItem(e) {
+				var index = e.currentTarget.id
+				var url = this.productList[index].link
+				window.location.href = url
+			},
 		}
 	}
 </script>
@@ -64,7 +104,45 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		justify-content: center;
+		align-content: center;
+		background-color: #2C405A;
+	}
+
+	.bg {
+		position: fixed;
+		width: 100%;
+		height: 100%;
+		background-color: #2C405A;
+	}
+
+	.grid {
+
+		margin-top: 0%;
+		margin-left: 5%;
+		margin-right: 5%;
+		background-color: #FFA72B;
+	}
+
+	.text_select {
+		color: #FFFFFF;
+		background-color: #ffa72b;
+	}
+
+	.item_view {
+		display: flex;
+		width: 100%;
+		height: 100%;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.item_icon {
+		width: 100%;
+		height: 100%;
+	}
+
+	.item_title {
+		color: #FFFFFF;
 	}
 
 	.logo {
@@ -83,12 +161,12 @@
 
 	.title {
 		font-size: 36rpx;
-		color: #8f8f94;
+		color: #FFFFFF;
 	}
 
 	.bottom {
 		display: flex;
-		flex-direction: row;
+		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 		position: fixed;
@@ -103,6 +181,19 @@
 		font-size: 20upx;
 		padding-left: 5upx;
 		padding-right: 5upx;
+		color: #FFFFFF;
+		text-decoration: underline;
+
+	}
+
+	.copyright {
+		text-underline: initial;
+		text-size-adjust: inherit;
+		font-size: 20upx;
+		margin-top: 30upx;
+		padding-left: 5upx;
+		padding-right: 5upx;
+		color: #999999;
 		text-decoration: underline;
 
 	}
